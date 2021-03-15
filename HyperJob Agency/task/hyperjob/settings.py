10f8c1b9a1369ff7b75718a5c7bbb4d144b1745b
supermads <madisonsfahey@gmail.com>
2021-03-15 11:36:00 -0700

    Stage 2 of Project

--- HyperJob Agency/task/hyperjob/settings.py
+++ HyperJob Agency/task/hyperjob/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'resume',
     'vacancy',
+    'menu'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'hyperjob.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- HyperJob Agency/task/hyperjob/urls.py
+++ HyperJob Agency/task/hyperjob/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from menu.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view())
 ]
